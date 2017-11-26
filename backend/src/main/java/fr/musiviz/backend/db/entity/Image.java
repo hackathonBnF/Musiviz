1adfe4e354d1da3157720a5f7f4640219e613127
kemkem <marc@kprod.net>
2017-11-26 04:57:28 +0100

    json ignore audiorecard from image request

--- backend/src/main/java/fr/musiviz/backend/db/entity/Image.java
+++ backend/src/main/java/fr/musiviz/backend/db/entity/Image.java
@@ -1,5 +1,7 @@
 package fr.musiviz.backend.db.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 /**
@@ -33,6 +35,7 @@ public class Image {
         this.url = url;
     }
 
+    @JsonIgnore
     @ManyToOne
     public AudioRecord getAudioRecord() {
         return audioRecord;
